92eea33c17d52f2feed901bbe05b347d3226047a
a11ce <a11ce@protonmail.com>
2017-02-24 09:48:18 -0500

    First commit

---
+++ kat-the-bot/src/main/java/com/a11ce/maven/kat_the_bot/App.java
@@ -0,0 +1,13 @@
+package com.a11ce.maven.kat_the_bot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Git!" );
+    }
+}
